2615ebbdb07486a77a89b682c71bf4fb5522739c
Geonhee Son <ryan.son1002@gmail.com>
2022-06-20 22:54:19 +0900

    Initial Commit

---
+++ WeatherScrollingSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherScrollingSample
+//
+//  Created by Geonhee on 2022/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherScrollingSample/WeatherScrollingSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherScrollingSampleApp.swift
+//  WeatherScrollingSample
+//
+//  Created by Geonhee on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherScrollingSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
